7bd9f354305987b9091e6fbf10e7ef6a6394a993
justice <justice@localhost>
2015-05-31 22:49:22 -0400

    missed postMeta in prev commit

---
+++ Blogg/Models/PostMeta.cs
@@ -0,0 +1,11 @@
+﻿namespace Blogg.Models
+{
+    public class PostMeta
+    {
+        public int ID { get; set; }
+        public int PostID { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public virtual Post Post { get; set; }
+    }
+}
